c1b9cf176e523d24a92a0323f67377e131f3bb5c
1Nia1 <108652120+1Nia1@users.noreply.github.com>
2022-07-21 14:56:27 +0800

    Create script.js
    java script for digital alarm

---
+++ script.js
@@ -0,0 +1,45 @@
+#timer {
+  width: 460px;
+  font-size: 70px;
+  color: aquamarine;
+  margin-left:150px;
+  padding: 10px;
+  font-family: "Lucida Handwriting", "Brush script MT", cursive;
+}
+
+
+#controls {
+  margin-left:150px;
+
+}
+
+button {
+  width: 100px;
+  height: 40px;
+  float: left;
+  margin-left: 10px;
+  font-size: 12px;
+  font-weight: bold;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px #888888;
+}
+
+
+body, button {
+  font-family: 'Courier';
+  font-family: 'digital-7', sans-serif;
+}
+
+{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  height: 100%;
+  background: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
